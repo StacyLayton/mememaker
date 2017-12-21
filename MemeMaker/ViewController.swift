11df88b9b215747028fae6b8713a00646a88d47d
Stacy Layton <stacy.layton@familysearch.org>
2017-12-21 10:38:34 -0700

    Added more photos. Added 3rd lines to Top and Bottom for meme phrases.

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -15,26 +15,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var TopCaptionSegControl: UISegmentedControl!
     @IBOutlet weak var Top2ndCaptionSegControl: UISegmentedControl!
+    @IBOutlet weak var Top3rdCaptionSegControl: UISegmentedControl!
     @IBOutlet weak var TopCaptionLabel: UILabel!
     
     @IBOutlet weak var BottomCaptionLabel: UILabel!
+    @IBOutlet weak var Bottom3rdCaptionSegControl: UISegmentedControl!
     @IBOutlet weak var Bottom2ndCaptionSegControl: UISegmentedControl!
     @IBOutlet weak var BottomCaptionSegControl: UISegmentedControl!
     
     var selectPhotoChoices = [ImageSelector]()
     var topChoices = [CaptionChoice]()
     var top2ndChoices = [CaptionChoice]()
+    var top3rdChoices = [CaptionChoice]()
     var bottomChoices = [CaptionChoice]()
     var bottom2ndChoices = [CaptionChoice]()
+    var bottom3rdChoices = [CaptionChoice]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
         ///Buttons to scroll through Images
-        let blankImage = ImageSelector(emoji: "۫", photo: "babyface1.jpg")
-        let previousImage = ImageSelector(emoji: "⬅", photo: "craftykid1.jpg")
-        let nextImage = ImageSelector(emoji: "➡", photo: "craftykid2.jpg")
+        let blankImage = ImageSelector(emoji: "۫", photo: "blankimage.jpg")
+        let previousImage = ImageSelector(emoji: "⬅", photo: "blankimage.jpg")
+        let nextImage = ImageSelector(emoji: "➡", photo: "blankimage.jpg")
         
         selectPhotoChoices = [previousImage, blankImage, nextImage]
         PhotoSelectorControl.removeAllSegments()
@@ -70,23 +74,52 @@ class ViewController: UIViewController {
         let meanwhileChoice = CaptionChoice(emoji: "☄️", caption: "MEANWHILE") //19
         let funChoice = CaptionChoice(emoji: "🍄", caption: "I HAD FUN ONCE") //20
         let notSimplyChoice = CaptionChoice(emoji: "🚫", caption: "ONE DOES NOT SIMPLY") //21
-        //let dontAlwaysChoice = CaptionChoice(emoji: "🔝", caption: "I DON'T ALWAYS")
         let oneMoreChoice = CaptionChoice(emoji: "🕐", caption: "ONE MORE TIME") //22
         
-        topChoices = [blankOption, likeMeChoice, madChoice, omgChoice, failChoice, lolChoice, ninjaChoice, moodChoice, goodDealChoice, fatChoice, introChoice, awkwardChoice]
-        top2ndChoices = [blankOption, gotThisChoice, basicallyChoice, smileChoice, troubleChoice, watchChoice, toldChoice, noteChoice, meanwhileChoice, funChoice, notSimplyChoice, oneMoreChoice]
+        let arnoldThisChoice = CaptionChoice(emoji: "🔙", caption: "I'LL BE BACK") //23
+        let worthChoice = CaptionChoice(emoji: "💵", caption: "WORTH IT") //24
+        let whyChoice = CaptionChoice(emoji: "🤷🏻‍♀️", caption: "THIS IS WHY") //25
+        let ripChoice = CaptionChoice(emoji: "💀", caption: "RIP") //26
+        let greatChoice = CaptionChoice(emoji: "🦈", caption: "THAT WOULD BE GREAT") //27
+        let struggleChoice = CaptionChoice(emoji: "😫", caption: "THE STRUGGLE IS REAL") //28
+        let tellMeChoice = CaptionChoice(emoji: "🗣", caption: "YOU MEAN TO TELL ME") //29
+        let understandChoice = CaptionChoice(emoji: "👭", caption: "YOU UNDERSTAND ME") //30
+        let peeChoice = CaptionChoice(emoji: "🚽", caption: "I HAVE TO PEE") //31
+        let ninjaCatChoice = CaptionChoice(emoji: "🐱", caption: "NINJA CAT") //32
+        let someoneSaysChoice = CaptionChoice(emoji: "✋🏾", caption: "WHEN SOMEONE SAYS") //33
+        
+        topChoices = [blankOption, likeMeChoice, madChoice, omgChoice, failChoice, lolChoice, ninjaChoice, moodChoice, goodDealChoice, fatChoice, introChoice, awkwardChoice, blankOption]
+        top2ndChoices = [blankOption, gotThisChoice, basicallyChoice, smileChoice, troubleChoice, watchChoice, toldChoice, noteChoice, meanwhileChoice, funChoice, notSimplyChoice, oneMoreChoice, blankOption]
+        top3rdChoices = [blankOption, arnoldThisChoice, worthChoice, whyChoice, ripChoice, greatChoice, struggleChoice, tellMeChoice, understandChoice, peeChoice, ninjaCatChoice, someoneSaysChoice, blankOption]
         TopCaptionSegControl.removeAllSegments()
         Top2ndCaptionSegControl.removeAllSegments()
+        Top3rdCaptionSegControl.removeAllSegments()
         for choice in topChoices {
             TopCaptionSegControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
         }
         for choice in top2ndChoices {
             Top2ndCaptionSegControl.insertSegment(withTitle: choice.emoji, at: top2ndChoices.count, animated: false)
         }
+        for choice in top3rdChoices {
+            Top3rdCaptionSegControl.insertSegment(withTitle: choice.emoji, at: top3rdChoices.count, animated: false)
+        }
         TopCaptionSegControl.selectedSegmentIndex = 0
         Top2ndCaptionSegControl.selectedSegmentIndex = 0
+        Top3rdCaptionSegControl.selectedSegmentIndex = 0
         
         ///BOTTOM
+        let lateChoice = CaptionChoice(emoji: "💥", caption: "TOO LATE!") //23
+        let eatedChoice = CaptionChoice(emoji: "🐼", caption: "I EATED IT") //24
+        let pokerChoice = CaptionChoice(emoji: "♠️", caption: "*poker face*") //25
+        let awfulChoice = CaptionChoice(emoji: "🤮", caption: "IT WAS AWFUL") //26
+        let caredChoice = CaptionChoice(emoji: "💔", caption: "IN MEMORY OF WHEN I CARED") //27
+        let lookinGoodChoice = CaptionChoice(emoji: "💅🏼", caption: "LOOKIN' THIS GOOD AIN'T EASY") //28
+        let deadInsideChoice = CaptionChoice(emoji: "☠️", caption: "WHEN UR DEAD INSIDE") //29
+        let moreChoice = CaptionChoice(emoji: "🙄", caption: "PLEASE TELL ME MORE") //30
+        let batChoice = CaptionChoice(emoji: "🦇", caption: "UNLESS YOU'RE BATMAN") //31
+        let unseenChoice = CaptionChoice(emoji: "😳", caption: "CANNOT BE UNSEEN") //32
+        let winningChoice = CaptionChoice(emoji: "🏆", caption: "#WINNING") //33
+        
         let catChoice = CaptionChoice(emoji: "😸", caption: "NO. JUST NO.") //12
         let wiseChoice = CaptionChoice(emoji: "🐼", caption: "CHOOSE WISELY") //13
         let epicChoice = CaptionChoice(emoji: "👑", caption: "EPIC") //14
@@ -95,9 +128,7 @@ class ViewController: UIViewController {
         let cowChoice = CaptionChoice(emoji: "🐮", caption: "I GOTTA HAVE MORE COWBELL!") //17
         let baseChoice = CaptionChoice(emoji: "🎮", caption: "ALL YOUR BASE ARE BELONG TO US") //18
         let terroristChoice = CaptionChoice(emoji: "💣", caption: "I KILL YOU") //19
-        //let batChoice = CaptionChoice(emoji: "🦇", caption: "UNLESS YOU'RE BATMAN")
         let classChoice = CaptionChoice(emoji: "🤪", caption: "SUCH CLASS") //20
-        //let secondChoice = CaptionChoice(emoji: "𝟐", caption: "BUT THEN I WAS LIKE")
         let thugChoice = CaptionChoice(emoji: "👨‍🎤", caption: "THUG LIFE") //21
         let wrongChoice = CaptionChoice(emoji: "🤢", caption: "I WAS WRONG!") //22
         
@@ -111,22 +142,28 @@ class ViewController: UIViewController {
         let problemChoice = CaptionChoice(emoji: "👮‍♀️", caption: "PROBLEM?") //8
         let acceptChoice = CaptionChoice(emoji: "🆗", caption: "ACCEPT IT") //9
         let wowChoice = CaptionChoice(emoji: "✨", caption: "SO MUCH WOW") //10
-        let trumpChoice = CaptionChoice(emoji: "🦇", caption: "WRONG") //11
+        let trumpChoice = CaptionChoice(emoji: "🤓", caption: "WRONG") //11
   
-        bottomChoices = [blankOption, catChoice, wiseChoice, epicChoice, kermitChoice, foxChoice, cowChoice, baseChoice, terroristChoice, classChoice, thugChoice, wrongChoice]
-        bottom2ndChoices = [blankOption, successChoice, urWorthChoice, faithChoice, terribleChoice, chillChoice, derpChoice, legitChoice, problemChoice, acceptChoice, wowChoice, trumpChoice]
+        bottomChoices = [blankOption, catChoice, wiseChoice, epicChoice, kermitChoice, foxChoice, cowChoice, baseChoice, terroristChoice, classChoice, thugChoice, wrongChoice, blankOption]
+        bottom2ndChoices = [blankOption, successChoice, urWorthChoice, faithChoice, terribleChoice, chillChoice, derpChoice, legitChoice, problemChoice, acceptChoice, wowChoice, trumpChoice, blankOption]
+        bottom3rdChoices = [blankOption, lateChoice, eatedChoice, pokerChoice, awfulChoice, caredChoice, lookinGoodChoice, deadInsideChoice, moreChoice, batChoice, unseenChoice, winningChoice, blankOption]
         BottomCaptionSegControl.removeAllSegments()
         Bottom2ndCaptionSegControl.removeAllSegments()
+        Bottom3rdCaptionSegControl.removeAllSegments()
         for choice in bottomChoices {
             BottomCaptionSegControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false)
         }
         for choice in bottom2ndChoices {
             Bottom2ndCaptionSegControl.insertSegment(withTitle: choice.emoji, at: bottom2ndChoices.count, animated: false)
         }
+        for choice in bottom3rdChoices {
+            Bottom3rdCaptionSegControl.insertSegment(withTitle: choice.emoji, at: bottom3rdChoices.count, animated: false)
+        }
         BottomCaptionSegControl.selectedSegmentIndex = 0
         Bottom2ndCaptionSegControl.selectedSegmentIndex = 0
+        Bottom3rdCaptionSegControl.selectedSegmentIndex = 0
         
-        //updateImage()
+        updateImage()
         updateTopLabel()
         updateBottomLabel()
     }
@@ -138,10 +175,12 @@ class ViewController: UIViewController {
     
     func updateImage() {
         ///UPDATE IMAGE
-        let imageIndex = PhotoSelectorControl.selectedSegmentIndex
-        let imageChoice = selectPhotoChoices[imageIndex]
-        let imageName = imageChoice.photo
-        MemeImage.image = UIImage(named: imageName)
+        let presetPhotos = ["babyface1", "craftykid1", "craftykid2", "craftykid3", "craftykid4", "craftykid5", "crying1", "crying2", "faces1", "faces2", "faces3", "faces4", "faces5", "faces6", "faces7", "funnycat1", "funnycat2", "funnycat3", "funnymonkey1", "kids1", "kids2", "kids3", "kids4", "kids5", "kids6", "kids7", "kids8", "kids9", "kids10", "kids11", "kids12", "kids13", "kids14","kids15", "kids16", "kids18", "kids19", "kids20", "kids21", "kids22", "kids23", "kids24", "kids25", "kids26", "kids27", "kids28", "kids29", "kids30", "kids31", "kids32", "kids33.png"]
+        
+        let randomNewPhoto = presetPhotos[Int(arc4random_uniform(UInt32(presetPhotos.count)))]
+        MemeImage.image = UIImage(named: randomNewPhoto)
+        //Reset the selector without updating the image
+        PhotoSelectorControl.selectedSegmentIndex = 1
     }
     
     func updateTopLabel() {
@@ -149,16 +188,22 @@ class ViewController: UIViewController {
         var topIndex: Int
         var topChoice: CaptionChoice
         //Only one choice from the top segments
-        if (TopCaptionSegControl.selectedSegmentIndex == 0) {
-            topIndex = (Top2ndCaptionSegControl.selectedSegmentIndex)
-            topChoice = top2ndChoices[topIndex]
-        } else {
+        if (TopCaptionSegControl.selectedSegmentIndex != 0) {
             topIndex = TopCaptionSegControl.selectedSegmentIndex
             topChoice = topChoices[topIndex]
+        }
+        else if (Top2ndCaptionSegControl.selectedSegmentIndex != 0) {
+            topIndex = (Top2ndCaptionSegControl.selectedSegmentIndex)
+            topChoice = top2ndChoices[topIndex]
+        }
+        else {
+            topIndex = (Top3rdCaptionSegControl.selectedSegmentIndex)
+            topChoice = top3rdChoices[topIndex]
         }
         //Reset these values
         TopCaptionSegControl.selectedSegmentIndex = 0
         Top2ndCaptionSegControl.selectedSegmentIndex = 0
+        Top3rdCaptionSegControl.selectedSegmentIndex = 0
         
         TopCaptionLabel.text = topChoice.caption
     }
@@ -168,16 +213,22 @@ class ViewController: UIViewController {
         var bottomIndex: Int
         var bottomChoice: CaptionChoice
         //Only one choice from the bottom segments
-        if (BottomCaptionSegControl.selectedSegmentIndex == 0) {
-            bottomIndex = (Bottom2ndCaptionSegControl.selectedSegmentIndex)
-            bottomChoice = bottom2ndChoices[bottomIndex]
-        } else {
+        if (BottomCaptionSegControl.selectedSegmentIndex != 0) {
             bottomIndex = BottomCaptionSegControl.selectedSegmentIndex
             bottomChoice = bottomChoices[bottomIndex]
+        }
+        else if (Bottom2ndCaptionSegControl.selectedSegmentIndex != 0) {
+            bottomIndex = (Bottom2ndCaptionSegControl.selectedSegmentIndex)
+            bottomChoice = bottom2ndChoices[bottomIndex]
+        }
+        else {
+            bottomIndex = (Bottom3rdCaptionSegControl.selectedSegmentIndex)
+            bottomChoice = bottom3rdChoices[bottomIndex]
         }
         //Reset these values
         BottomCaptionSegControl.selectedSegmentIndex = 0
         Bottom2ndCaptionSegControl.selectedSegmentIndex = 0
+        Bottom3rdCaptionSegControl.selectedSegmentIndex = 0
         
         BottomCaptionLabel.text = bottomChoice.caption
     }
